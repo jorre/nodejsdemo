d7c96735e3eb4bbc55664bf95d188c1cbcc07e32
Joris Hens <joris@goodbytes.be>
2022-03-14 16:55:06 +0100

    cors

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const express = require("express");
 const messagesRouter = require("./routes/api/v1/messages");
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.set("view engine", "pug");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.render("index", { title: "hey" });
-    app.use(cors());
 });
 app.use("/api/v1/messages", messagesRouter);
 
